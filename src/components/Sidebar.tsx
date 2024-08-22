95bc513052329b7d98caa93efc734c8a6565b151
daoleno <daodaoonline@gmail.com>
2024-08-22 14:03:57 +0800

    feat: add logo, adjust default window size, adjust sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,57 +1,53 @@
-import { Button } from "@/components/ui/button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useState } from "react";
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 interface SidebarItem {
-	id: string;
-	label: string;
-	icon: React.ElementType;
+  id: string
+  label: string
+  icon: React.ElementType
 }
 
 interface SidebarProps {
-	items: SidebarItem[];
-	activeTab: string;
-	setActiveTab: (id: string) => void;
+  items: SidebarItem[]
+  activeTab: string
+  setActiveTab: (id: string) => void
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-	items,
-	activeTab,
-	setActiveTab,
+  items,
+  activeTab,
+  setActiveTab,
 }) => {
-	const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className="bg-muted text-card-foreground p-4 w-64">
+      <div className="flex items-center mb-6 pl-2">
+        <img
+          src="/logo.svg"
+          alt="raybit"
+          width={32}
+          height={32}
+          className="mr-2"
+        />
+        <h1 className="text-2xl font-bold">RaaR</h1>
+      </div>
+      <div className="space-y-2">
+        {items.map((item) => (
+          <Button
+            key={item.id}
+            variant={'ghost'}
+            className={cn(
+              'w-full justify-start px-2 text-left',
+              activeTab === item.id && 'bg-accent',
+            )}
+            onClick={() => setActiveTab(item.id)}
+          >
+            <item.icon size={16} className="mr-2" />
+            <span>{item.label}</span>
+          </Button>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-	return (
-		<div
-			className={`bg-card text-card-foreground p-4 transition-all duration-300 ${
-				collapsed ? "w-16" : "w-64"
-			}`}
-		>
-			<div className="flex justify-between items-center mb-6">
-				{!collapsed && <h1 className="text-2xl font-bold">Dev Dashboard</h1>}
-				<Button
-					variant="ghost"
-					size="icon"
-					onClick={() => setCollapsed(!collapsed)}
-				>
-					{collapsed ? <ChevronRight size={24} /> : <ChevronLeft size={24} />}
-				</Button>
-			</div>
-			<div className="space-y-2">
-				{items.map((item) => (
-					<Button
-						key={item.id}
-						variant={activeTab === item.id ? "default" : "ghost"}
-						className={`w-full justify-start ${collapsed ? "px-2" : "px-4"}`}
-						onClick={() => setActiveTab(item.id)}
-					>
-						<item.icon size={24} className={collapsed ? "mr-0" : "mr-2"} />
-						{!collapsed && <span>{item.label}</span>}
-					</Button>
-				))}
-			</div>
-		</div>
-	);
-};
-
-export default Sidebar;
+export default Sidebar
